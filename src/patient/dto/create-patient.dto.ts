99bc08f6fb03ad89675a87a0b76277f0d83dce98
LiveiraGusta <gustavo.oliver456@gmail.com>
2025-11-29 17:21:11 -0300

    feat(patient): create Patient dto

--- src/patient/dto/create-patient.dto.ts
+++ src/patient/dto/create-patient.dto.ts
@@ -1,3 +1,19 @@
-export class CreatePatientDto {
+import { IsString, IsNotEmpty, IsDateString, Length, IsEmail } from 'class-validator';
+import { Patient } from 'src/generated/prisma/client';
 
+export class CreatePatientDto implements Partial<Patient>{
+    @IsString()
+    @IsNotEmpty({ message: 'Name is required' })
+    name: string;
+
+    @IsEmail()
+    email: string
+
+    @IsString()
+    phone: string
+
+    @IsString()
+    @IsNotEmpty({ message: 'Document is required' })
+    @Length(11, 11, { message: 'Doocument must have 11 characters' })
+    document: string;
 }
---
+++ src/patient/dto/query-patient.dto.ts
@@ -0,0 +1,28 @@
+import { IsOptional, IsInt, Min, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class QueryPatientsDto {
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    page?: number = 1;
+
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    pageSize?: number = 10;
+
+    @IsOptional()
+    @IsString()
+    search?: string;
+
+    @IsOptional()
+    @IsString()
+    name?: string;
+
+    @IsOptional()
+    @IsString()
+    document?: string;
+}
