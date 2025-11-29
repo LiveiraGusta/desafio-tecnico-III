1cc4a2a63dd7810569b71b3410faabaec6cf3a4c
LiveiraGusta <gustavo.oliver456@gmail.com>
2025-11-29 14:37:53 -0300

    fix(prisma): make prisma global and fix missing exports

--- src/shared/prisma/prisma.service.ts
+++ src/shared/prisma/prisma.service.ts
@@ -1,14 +1,25 @@
-import { Injectable, OnModuleInit, OnModuleDestroy, INestApplication } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma/client';
-
-
+import { Injectable, OnModuleInit, OnModuleDestroy, INestApplication, Logger } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
+import "dotenv/config";
+ 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy{
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor(){
+    const adapter = new PrismaPg({connectionString : process.env.DATABASE_URL });
+    super({adapter});
+  }
+
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$queryRaw`SELECT 1`;
+      this.logger.log('Database connection established successfully');
+    } catch (error) {
+      this.logger.error('Erro ao conectar no banco de dados', error.stack);
+      throw error; 
+    }
   }
   
   async onModuleDestroy() {
