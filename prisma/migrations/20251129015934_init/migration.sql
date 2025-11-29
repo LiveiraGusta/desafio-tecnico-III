3e23ca4e8076bb6320fd914eb50d24742d1edb80
LiveiraGusta <gustavo.oliver456@gmail.com>
2025-11-28 23:15:22 -0300

    feat(database): add patients and exams tables

---
+++ prisma/migrations/20251129015934_init/migration.sql
@@ -0,0 +1,44 @@
+-- CreateEnum
+CREATE TYPE "DicomModality" AS ENUM ('CR', 'CT', 'DX', 'MG', 'MR', 'NM', 'OT', 'PT', 'RF', 'US', 'XA');
+
+-- CreateTable
+CREATE TABLE "Patient" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "document" TEXT NOT NULL,
+    "birthDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Patient_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Exam" (
+    "id" TEXT NOT NULL,
+    "patientId" TEXT NOT NULL,
+    "dicomModality" "DicomModality" NOT NULL,
+    "examDate" TIMESTAMP(3) NOT NULL,
+    "idempotencyKey" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Exam_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Patient_document_key" ON "Patient"("document");
+
+-- CreateIndex
+CREATE INDEX "Patient_document_idx" ON "Patient"("document");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Exam_idempotencyKey_key" ON "Exam"("idempotencyKey");
+
+-- CreateIndex
+CREATE INDEX "Exam_patientId_idx" ON "Exam"("patientId");
+
+-- CreateIndex
+CREATE INDEX "Exam_idempotencyKey_idx" ON "Exam"("idempotencyKey");
+
+-- AddForeignKey
+ALTER TABLE "Exam" ADD CONSTRAINT "Exam_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "Patient"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
