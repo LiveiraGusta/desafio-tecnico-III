35324774f39bb592012d25ec85ba3c92a06661fe
LiveiraGusta <gustavo.oliver456@gmail.com>
2025-11-30 14:21:39 -0300

    refactor(routes): remove auxiliary routes

--- frontend/src/app/features/exames/exames.routes.ts
+++ frontend/src/app/features/exames/exames.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
 import { ExamesList } from './exames-list/exames-list';
-import { ExamesForm } from './exames-form/exames-form';
 
 export const EXAMES_ROUTES: Routes = [
   { path: '', component: ExamesList},
-  { path: 'novo', component: ExamesForm},
-  { path: ':id/editar', component: ExamesForm},
 ];
